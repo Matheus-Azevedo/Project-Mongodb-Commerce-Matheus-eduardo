2fbbd5b00c1f4e994ce274204460197765621303
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-02-07 22:59:40 -0300

    FEAT: Requisito 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,6 @@
+db.createCollection("resumoProdutos");
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.count({}),
+});
+db.resumoProdutos.find({}, { _id: 0 });
