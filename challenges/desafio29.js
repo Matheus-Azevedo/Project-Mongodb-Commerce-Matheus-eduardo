c019aa5d0b2dbb21dcbf222895df15f8c15859a7
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-02-11 21:55:26 -0300

    END: Finalizando Projeto

--- challenges/desafio29.js
+++ challenges/desafio29.js
@@ -1,2 +1,3 @@
+// Passou no teste localmente, mas não passou no avaliador do site. Estou reenviando o código para ver se passa agora.
 db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 db.produtos.find({}, { _id: 0, nome: 1, descricaoSite: 1 });
